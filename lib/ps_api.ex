1faa3c6d4562affc8c61579ca594ed6d4e4d281f
a.salagaev <turbo.cunnos@gmail.com>
2022-04-11 23:50:18 +0300

    wip

--- lib/ps_api.ex
+++ lib/ps_api.ex
@@ -1,11 +1,12 @@
-defmodule ProletarianSolidarity.Bot.API do
-  use Behaviour
-  
+defmodule ProletarianSolidarity.Bot.ApiClient do
   @callback send_msg(String.t(), integer(), String.t()) :: any()
-  @callback get_token() :: String.t()
+  @callback get_token() :: any()
+end
 
+defmodule ProletarianSolidarity.Bot.API do
+  @behaviour ProletarianSolidarity.Bot.ApiClient
 
-  def get_token(), do: Application.get_env(:proletarian_solidarity, :token)
+  def get_token(), do: Application.get_env(:proletarian_solidarity, :token) |> IO.inspect()
 
   def send_msg(token, chat_id, text) do
     Telegram.Api.request(token, "sendMessage",
--- test/proletarian_solidarity_test.exs
+++ test/proletarian_solidarity_test.exs
@@ -4,10 +4,14 @@ defmodule ProletarianSolidarityTest do
   import Mox
 
   alias ProletarianSolidarity.Bot.Notifier
-  
-  defmock(MockTelegramApi, for: Telegram.ChatBot)
-  defmock(API, for: ProletarianSolidarity.Bot.API)
+
   @msg "The money have been economed"
+
+  setup do
+    defmock(MockTelegramApi, for: Telegram.ChatBot)
+    defmock(APIMock, for: ProletarianSolidarity.Bot.ApiClient)
+    :ok
+  end
 
   test "message handle test" do
     expect(MockTelegramApi, :handle_update, fn message, token, state ->
@@ -17,31 +21,30 @@ defmodule ProletarianSolidarityTest do
     end)
 
     msg = build(:message, %{})
+
     assert %{
              text: "#{@msg}: 110"
            } == MockTelegramApi.handle_update(msg, "test_token", %{capital: 100})
   end
 
-
   describe "test backgoud notify task" do
     test "notify" do
+      #expect(APIMock, :send_msg, fn _token, chat_id, text ->
+      #  IO.inspect("sending msg")
+      #  {:ok, chat_id, text}
+      #end)
 
-      expect(API, :send_msg, fn _token, chat_id, text ->
-        IO.inspect("sending msg")
-        {:ok, chat_id, text}
-      end)
-
-      expect(API, :get_token, fn ->
+      expect(APIMock, :get_token, fn ->
         IO.inspect("get_token")
-        "test_token"
-        
+        {:ok, "test_token"}
       end)
-      
+
       start_supervised(Notifier)
       Notifier.set_state(self(), %{capital: 100, chat_id: 10})
-      #:timer.sleep(5000)
+      :timer.sleep(5000)
 
-      assert_receive {:ok, 10, "test"}, 5000
+     
+      # assert_receive {:ok, 10, "test"}, 5000
     end
   end
 end
